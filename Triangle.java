f9d2ec770d3d45c78107abc464259fa3c31af8b7
unknown <omer.lasry@post.runi.ac.il>
2023-12-23 13:11:17 +0200

    fixed program

--- Triangle.java
+++ Triangle.java
@@ -10,7 +10,7 @@ public class Triangle {
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
 		int c = Integer.parseInt(args[2]);
-		boolean test = (a+b < c && a+c < b && b+c<a);
+		boolean test = (a+b > c && a+c > b && b+c>a);
 		System.out.println(a + ", " + b + ", " + c + ": " + test);
 	}
 }
