22d226640fcdf3d93a14a867bbdb7e7d55849f9e
unknown <omer.lasry@post.runi.ac.il>
2023-12-23 12:12:19 +0200

    fixed program

--- LinearEq.java
+++ LinearEq.java
@@ -12,13 +12,9 @@ public class LinearEq
 	double a = Integer.parseInt(args[0]);
 	double b = Integer.parseInt(args[1]);
 	double c = Integer.parseInt(args[2]);
-	double x1,x2;
-	x1 = (-b + Math.sqrt(b*b-(4*a*c)))/ 2*a;
-	x2 = (-b - Math.sqrt(b*b-(4*a*c)))/ 2*a;
-	if(x1 == x2 )
-		System.out.println("x1 is " + x1 );
-	else
-		System.out.println("x1 is " + x1 + " x2 is " +x2);
+	double x = (c-b)/a;
+	System.out.println(a + "x " + " + "+ b  +  " = " +c);
+	System.out.println(" x is " +a);
 	}
 	
 }
